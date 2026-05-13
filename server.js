f78b22cef4371955010a8d34460fef3cbcae34da
keepcold-fr <contact@keepcold.fr>
2026-05-13 23:15:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1771,7 +1771,7 @@ console.log("LONGUEUR CLE :", cle.length);
       Exp_Devise: "EUR",
 
       COL_Rel_Pays: "FR",
-      COL_Rel: "03559",
+      COL_Rel: "AUTO",
       LIV_Rel_Pays: "FR",
       LIV_Rel: "039559",
 
